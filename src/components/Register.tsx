6d989a4977f346b7d79e42d78a805a5a0540210a
Karina <karina.kvych@gmail.com>
2024-01-28 22:03:36 +0000

    fix

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,4 +1,12 @@
-import { Button, FormControl, Input } from "@chakra-ui/react"
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react"
 import { useMemo, useState } from "react"
 import { useMutation } from "react-query"
 import { useNavigate, useSearchParams } from "react-router-dom"
@@ -12,25 +20,31 @@ export default function Register() {
   const [last_name, setLastName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [repeatPassworld, setRepeatedPassworld] = useState("")
+  const [showPassword, setShow] = useState(false)
+  const [repeatPassword, setRepeatedPassword] = useState("")
   const [_, setSearchParams] = useSearchParams()
 
   const navigate = useNavigate()
 
   function validateName(type: "first_name" | "last_name") {
     const name = type === "first_name" ? first_name : last_name
-    return rules.required(name) && rules.nameMinLength(name) && rules.nameMaxLength(name)
+    return (
+      rules.required(name) &&
+      rules.nameMinLength(name) &&
+      rules.nameMaxLength(name)
+    )
   }
   function validateEmail() {
-    return rules.email(email) && rules.emailMaxLength(email)
+    return (
+      rules.required(email) && rules.email(email) && rules.emailMaxLength(email)
+    )
   }
   function validatePassword() {
     return rules.strongPasswordCheck(password)
   }
   function validateRepeatPassword() {
-    return rules.comparePasswords(password, repeatPassworld)
+    return rules.comparePasswords(password, repeatPassword)
   }
-  console.log(password, repeatPassworld)
 
   const isFormValid = useMemo(() => {
     const validations = [
@@ -41,7 +55,7 @@ export default function Register() {
       validateRepeatPassword(),
     ]
     return validations.every((v) => !!v && typeof v === "boolean")
-  }, [first_name, last_name, email, password])
+  }, [first_name, last_name, email, password, repeatPassword])
 
   const mutation = useMutation({
     mutationFn: api.Auth.registerUser,
@@ -71,23 +85,17 @@ export default function Register() {
     <form
       style={{
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         minWidth: "500px",
+        gap: "15px",
       }}
       onSubmit={submitData}>
       <FormControl
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        gap={15}
-        w="100%"
-        padding="10px"
-        color="black"
-        background="#f5f2fe"
-        boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
-        borderRadius="10px">
+        isRequired
+        isInvalid={!validateName("first_name")}>
+        <FormLabel>First name</FormLabel>
         <Input
           type="text"
           id="firstName"
@@ -96,63 +104,96 @@ export default function Register() {
           value={first_name}
           onChange={(e) => setFirstName(e.target.value)}
         />
-        <Input
-          type="text"
-          id="secondName"
-          focusBorderColor="#b4b1b0"
-          placeholder="Enter your last name"
-          value={last_name}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <Input
-          type="email"
-          id="email"
-          focusBorderColor="#b4b1b0"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type="password"
-          id="password"
-          focusBorderColor="#b4b1b0"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Input
-          type="password"
-          id="repeatPassword"
-          focusBorderColor="#b4b1b0"
-          placeholder="Repeat your password"
-          value={repeatPassworld}
-          onChange={(e) => setRepeatedPassworld(e.target.value)}
-        />
-        {mutation.isError ? (
-          <span>Error happend</span>
-        ) : (
-          <Button
-            type="submit"
-            id="submit"
-            background="#caf0f8"
-            color="#4f6b7c"
-            isLoading={mutation.isLoading}
-            isDisabled={!isFormValid}
-            fontSize="25px">
-            Register
-          </Button>
+        {!validateName("first_name") && (
+          <FormErrorMessage>Please enter your name</FormErrorMessage>
         )}
-
-        <Button
-          onClick={handlePageSwitch}
-          style={{
-            color: "#4f6b7c",
-            fontFamily: "cursive",
-            fontSize: "20px",
-          }}>
-          Already have an account
-        </Button>
       </FormControl>
+      <Input
+        type="text"
+        id="secondName"
+        focusBorderColor="#b4b1b0"
+        placeholder="Enter your last name"
+        value={last_name}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <Input
+        type="email"
+        id="email"
+        focusBorderColor="#b4b1b0"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <FormControl
+        isRequired
+        isInvalid={!validateName("first_name")}>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            focusBorderColor="#b4b1b0"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={() => setShow((p) => (p = !p))}>
+              {showPassword ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        {!validatePassword() && (
+          <FormErrorMessage>Please enter your password</FormErrorMessage>
+        )}
+      </FormControl>
+      <Input
+        type="password"
+        id="repeatPassword"
+        focusBorderColor="#b4b1b0"
+        placeholder="Repeat your password"
+        value={repeatPassword}
+        onChange={(e) => setRepeatedPassword(e.target.value)}
+      />
+      {mutation.isError ? (
+        <span>Error happend</span>
+      ) : (
+        <Button
+          type="submit"
+          id="submit"
+          background="#caf0f8"
+          color="#4f6b7c"
+          isLoading={mutation.isLoading}
+          isDisabled={!isFormValid}
+          fontSize="25px">
+          Register
+        </Button>
+      )}
+
+      <Button
+        onClick={handlePageSwitch}
+        style={{
+          color: "#4f6b7c",
+          fontFamily: "cursive",
+          fontSize: "20px",
+        }}>
+        Already have an account
+      </Button>
     </form>
   )
 }
+
+// display="flex"
+// flexDirection="column"
+// alignItems="center"
+// justifyContent="center"
+// gap={15}
+// w="100%"
+// padding="10px"
+// color="black"
+// background="#f5f2fe"
+// boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
+// borderRadius="10px"
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,16 +1,16 @@
 import { isEmail } from "validator"
 
 export const rules = {
-  required: (val: string): boolean | string => !!val || "Required",
-  email: (val: string): boolean | string => (!val.length ? true : isEmail(val)),
+  required: (val: string): boolean | string => !!val.length,
+  email: (val: string): boolean | string => isEmail(val),
   emailMaxLength: (val: string): boolean | string => val?.length < 100,
-  nameMinLength: (val: string) => val.length > 2,
+  nameMinLength: (val: string) => val.length > 1,
   nameMaxLength: (val: string) => val.length < 40,
   strongPasswordCheck: (password: string): boolean | string => {
-    const checking = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    return checking.test(password) || "Password is too weak"
+    const checking =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
+    return checking.test(password)
   },
-  comparePasswords: (password: string, repeatPassword: string): boolean | string =>
-    password === repeatPassword || "Your password doesn't match",
+  comparePasswords: (password: string, repeatPassword: string): boolean =>
+    !!password.length && password === repeatPassword,
 }
-
